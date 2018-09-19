af5624b2c44c505db6991ef6570dbb40a86b75bd
Cooperwd <40800061+Cooperwd@users.noreply.github.com>
2018-09-19 13:16:29 +0800

    9/19

---
+++ src/com/itcast/Hello.java
@@ -0,0 +1,11 @@
+package com.itcast;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.print("hello");
+        System.out.print("word");
+        System.out.println("hfjhdj");
+
+    }
+
+}
